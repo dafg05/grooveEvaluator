46cd4fd5bce5b5307bad0d4591b22a704df1d944
dafg05 <danialefloresg@gmail.com>
2024-04-19 17:41:57 -0400

    prettier multiple distance metrics plots

--- grooveEvaluator/plotting.py
+++ grooveEvaluator/plotting.py
@@ -40,51 +40,7 @@ def plot_distance_metrics(results: Dict[str, ComparisonResult], out_dir: Path, f
     plt.savefig(out_dir / f"{figname}_plot.png", dpi=300, bbox_inches='tight')
     plt.close()
 
-def plot_multiple_distance_metrics(results_1: Dict[str, ComparisonResult], results_2: Dict[str, ComparisonResult], setname_1: str, setname2: str, out_dir: Path, figname: str = "Distance Metrics", x_right_limit: float=-1, colors: List[str] = None):
-    features_1 = list(results_1.keys())
-    kl_divergences_1 = [result.kl_divergence for result in results_1.values()]
-    overlapping_areas_1 = [result.overlapping_area for result in results_1.values()]
-
-    features_2 = list(results_2.keys())
-    kl_divergences_2 = [result.kl_divergence for result in results_2.values()]
-    overlapping_areas_2 = [result.overlapping_area for result in results_2.values()]
-
-    if not colors:
-        # Using a more distinct color palette
-        colors = plt.get_cmap('tab20').colors
-    if x_right_limit < 0:
-        x_right_limit = max(kl_divergences_1 + kl_divergences_2) + 0.01 * max(kl_divergences_1 + kl_divergences_2)
-
-    y_bottom_limit = min(0, min(overlapping_areas_1 + overlapping_areas_2) - 0.01 * min(overlapping_areas_1 + overlapping_areas_2))
-
-    plt.figure(figsize=(12, 8))
-
-    # Plotting the first set of results with circles
-    for i, feature in enumerate(features_1):
-        plt.scatter(kl_divergences_1[i], overlapping_areas_1[i], color=colors[i % len(colors)], marker='o', label=f"{feature} ({setname_1})")
-
-    # Plotting the second set of results with squares
-    for i, feature in enumerate(features_2):
-        plt.scatter(kl_divergences_2[i], overlapping_areas_2[i], color=colors[i % len(colors)], marker='s', label=f"{feature} ({setname2})")
-
-    plt.xlabel('KL-D')
-    plt.ylabel('OA')
-    plt.title(figname)
-
-    plt.axhline(y=0, color='black', linewidth=0.5)
-    plt.axvline(x=0, color='black', linewidth=0.5)
-
-    plt.xlim(left=0, right=x_right_limit)
-    plt.ylim(bottom=y_bottom_limit, top=1.0)
-
-    # Improve legend placement and ensure it's not cut off
-    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left", title="Features", fontsize='small', markerscale=1)
-    plt.tight_layout(rect=[0, 0, 0.85, 1])  # Adjust the rect layout to make room for the legend
-
-    plt.savefig(out_dir / f"{figname}_plot.png", dpi=300, bbox_inches='tight')
-    plt.close()
-
-def plot_multiple_distance_metrics_adjusted(results_1, results_2, setname_1, setname_2, out_dir, figname="Distance Metrics", x_right_limit=-1, colors=None):
+def plot_multiple_distance_metrics(results_1, results_2, setname_1, setname_2, out_dir, figname="Distance Metrics", x_right_limit=-1, colors=None):
     if not colors:
         colors = plt.get_cmap('tab10').colors
 
@@ -92,7 +48,6 @@ def plot_multiple_distance_metrics_adjusted(results_1, results_2, setname_1, set
     max_kl_divergence = 0
     min_overlapping_area = 0
 
-    marker_size = 100
     # Plot with specified markers
     for i, (feature, result) in enumerate(results_1.items()):
         plt.scatter(result.kl_divergence, result.overlapping_area, color=colors[i], marker='v')  # Triangles for set 1
@@ -118,7 +73,7 @@ def plot_multiple_distance_metrics_adjusted(results_1, results_2, setname_1, set
 
     plt.xlabel('KL-Divergence', fontweight='bold')
     plt.ylabel('Overlapping Area', fontweight='bold')
-    plt.title(figname, fontweight='bold', fontsize = 14)  # Increase padding to ensure space for the annotation
+    plt.title(figname, fontweight='bold', fontsize = 14)
 
    # Annotations for set markers
     plt.text(0.5, -0.2, f'"{setname_1}" ▼ | "{setname_2}" ●', ha='center', va='center', 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grooveEvaluator',
-    version='0.1.10',
+    version='0.1.11',
     author='Daniel Flores',
     description="",
     long_description=open('README.md').read(),
