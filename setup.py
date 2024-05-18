b4904b8afcca21d69c605da829cf5d4b7ad37ebf
dafg05 <danialefloresg@gmail.com>
2024-05-17 22:05:58 -0400

    up version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grooveEvaluator',
-    version='0.1.16',
+    version='0.1.17',
     author='Daniel Flores',
     description="",
     long_description=open('README.md').read(),
